d9ffc86ffbf6d0b53f2b7754fe90e195d00ffb33
AlisonWolfbr <alison.luan.gomes@hotmail.com>
2025-10-07 17:16:53 -0300

    mudança no git ignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'twp8ho',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
   },
 });
